d738078065f4518d3d654ff4d20acb6459fcca11
Kyle Schulman <kyleschulman@Kyle-MBP.local>
2020-03-10 17:49:59 -0700

    Sort of working

--- graphics.js
+++ graphics.js
@@ -1,43 +1,64 @@
-        var box1 = document.getElementById("box1");
-        var box2 = document.getElementById("box2");
-        var box3 = document.getElementById("box3");
+var img = document.createElement("img");
 
-        box1.addEventListener('click', (event) => {
-            changeColor1(event);
-        });
-        box2.addEventListener('click', (event) => {
-            changeColor2(event);
-        });
-        box3.addEventListener('click', (event) => {
-            changeColor3(event);
-        });
+var img2 = document.createElement("img");
 
-        function changeColor1(event) {
-            let box1 = event.target
-            let color1 = box1.style.backgroundColor;
-            if (color == "red") {
-                box1.style.backgroundColor = "black";
-            } else {
-                box1.style.backgroundColor = "red";
-            }
-        }
+var but = document.getElementById("my_button");
 
-        function changeColor2(event) {
-            let box2 = event.target
-            let color2 = box2.style.backgroundColor;
-            if (color == "white") {
-                box2.style.backgroundColor = "red";
-            } else {
-                box2.style.backgroundColor = "white";
-            }
-        }
+but.addEventListener("mouseover", ()=>{
+ rollDice();
+})
 
-        function changeColor3(event) {
-            let box3 = event.target
-            let color3 = box3.style.backgroundColor;
-            if (color == "green") {
-                box3.style.backgroundColor = "yellow";
-            } else {
-                box3.style.backgroundColor = "green";
-            }
-        }
+let bro = document.getElementById("dice");
+
+bro.addEventListener("dblclick",()=> {
+ rollDice();
+})
+
+
+function rollDice(){
+var id = document.getElementById("dice");
+document.body.appendChild(img);
+document.body.appendChild(img2);
+var randomNum = Math.floor(Math.random() * 6);
+var randomNum2 = Math.floor(Math.random() * 6);
+var dicepic = ["https://upload.wikimedia.org/wikipedia/commons/2/2c/Alea_1.png","https://upload.wikimedia.org/wikipedia/commons/b/b8/Alea_2.png","https://upload.wikimedia.org/wikipedia/commons/2/2f/Alea_3.png","https://upload.wikimedia.org/wikipedia/commons/8/8d/Alea_4.png","https://upload.wikimedia.org/wikipedia/commons/5/55/Alea_5.png", "https://upload.wikimedia.org/wikipedia/commons/f/f4/Alea_6.png"];
+
+id.appendChild(img);
+img.setAttribute("src", dicepic[randomNum]);
+img2.setAttribute("src", dicepic[randomNum2]);
+id.appendChild(img2);
+
+}
+
+
+document.getElementById("dice").onclick = function(){
+	document.getElementById("output").style.color = 'blue';
+  document.getElementById("output2").style.color = '#73C6B6 ';
+}
+document.getElementById("dice").ondblclick = function(){
+	document.getElementById("output").style.color = 'green';
+  document.getElementById("output2").style.color = 'red';
+}
+
+document.getElementById("my_button").onclick = function(){
+	document.getElementById("output").style.color = 'red';
+  document.getElementById("output2").style.color = 'red';
+}
+document.getElementById("my_button").onmouseover = function(){
+	document.getElementById("output2").style.color = 'aqua';
+  document.getElementById("output").style.color = 'aqua';
+}
+
+window.addEventListener("keypress", changeColor);
+function changeColor(event) {
+    if (event.key === "b"){
+       document.getElementById("output").style.color = '#33FFB5';
+  document.getElementById("output2").style.color = '#F09FDD';
+    } else if (event.key === "o") {
+
+    } else {
+
+    }
+}
+
+
