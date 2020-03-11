eb93cca10c113224ecc4b02dd985aa98f7289121
Kyle Schulman <kyleschulman@Kyle-MBP.local>
2020-03-10 21:29:25 -0700

    Beauty

--- graphics.js
+++ graphics.js
@@ -3,65 +3,64 @@ var img2 = document.createElement("img");
 
 let but = document.getElementById("my_button");
 
-but.addEventListener("mouseover", ()=>{
- rollDice();
+but.addEventListener("mouseover", () => {
+	rollDice();
 })
 let bro = document.getElementById("dice");
 
-bro.addEventListener("dblclick",()=> {
- rollDice();
+bro.addEventListener("dblclick", () => {
+	rollDice();
 })
 
 
-function rollDice(){
-var id = document.getElementById("dice");
-document.body.appendChild(img);
-document.body.appendChild(img2);
-var randomNum = Math.floor(Math.random() * 6);
-var randomNum2 = Math.floor(Math.random() * 6);
-var dicepic = ["https://upload.wikimedia.org/wikipedia/commons/2/2c/Alea_1.png","https://upload.wikimedia.org/wikipedia/commons/b/b8/Alea_2.png","https://upload.wikimedia.org/wikipedia/commons/2/2f/Alea_3.png","https://upload.wikimedia.org/wikipedia/commons/8/8d/Alea_4.png","https://upload.wikimedia.org/wikipedia/commons/5/55/Alea_5.png", "https://upload.wikimedia.org/wikipedia/commons/f/f4/Alea_6.png"];
+function rollDice() {
+	var id = document.getElementById("dice");
+	document.body.appendChild(img);
+	document.body.appendChild(img2);
+	var randomNum = Math.floor(Math.random() * 6);
+	var randomNum2 = Math.floor(Math.random() * 6);
+	var dicepic = ["https://upload.wikimedia.org/wikipedia/commons/2/2c/Alea_1.png", "https://upload.wikimedia.org/wikipedia/commons/b/b8/Alea_2.png", "https://upload.wikimedia.org/wikipedia/commons/2/2f/Alea_3.png", "https://upload.wikimedia.org/wikipedia/commons/8/8d/Alea_4.png", "https://upload.wikimedia.org/wikipedia/commons/5/55/Alea_5.png", "https://upload.wikimedia.org/wikipedia/commons/f/f4/Alea_6.png"];
 
-id.appendChild(img);
-img.setAttribute("src", dicepic[randomNum]);
-img2.setAttribute("src", dicepic[randomNum2]);
-id.appendChild(img2);
+	id.appendChild(img);
+	img.setAttribute("src", dicepic[randomNum]);
+	img2.setAttribute("src", dicepic[randomNum2]);
+	id.appendChild(img2);
 
 }
 
 
-document.getElementById("dice").onclick = function(){
+document.getElementById("dice").onclick = function () {
 	document.getElementById("output").style.color = 'blue';
-  document.getElementById("output2").style.color = '#73C6B6 ';
+	document.getElementById("output2").style.color = '#73C6B6 ';
 }
-document.getElementById("dice").ondblclick = function(){
+document.getElementById("dice").ondblclick = function () {
 	document.getElementById("output").style.color = 'green';
-  document.getElementById("output2").style.color = 'red';
+	document.getElementById("output2").style.color = 'red';
 }
 
-document.getElementById("my_button").onclick = function(){
+document.getElementById("my_button").onclick = function () {
 	document.getElementById("output").style.color = 'red';
-  document.getElementById("output2").style.color = 'red';
+	document.getElementById("output2").style.color = 'red';
 }
-document.getElementById("my_button").onmouseover = function(){
+document.getElementById("my_button").onmouseover = function () {
 	document.getElementById("output2").style.color = 'aqua';
-  document.getElementById("output").style.color = 'aqua';
+	document.getElementById("output").style.color = 'aqua';
 }
 
 window.addEventListener("keypress", changeColor);
+
 function changeColor(event) {
-    if (event.key === "a"){
-       document.getElementById("output").style.color = '#33FFB5';
-  document.getElementById("output2").style.color = '#F09FDD';
-      rollDice();
-    } else if (event.key === "l") {
-       document.getElementById("output").style.color = '#808B96';
-  document.getElementById("output2").style.color = '#808B96';
-      rollDice();
-    } else {
-       document.getElementById("output").style.color = 'black';
-  document.getElementById("output2").style.color = 'black';
-      rollDice();
-    }
+	if (event.key === "a") {
+		document.getElementById("output").style.color = '#33FFB5';
+		document.getElementById("output2").style.color = '#F09FDD';
+		rollDice();
+	} else if (event.key === "l") {
+		document.getElementById("output").style.color = '#808B96';
+		document.getElementById("output2").style.color = '#808B96';
+		rollDice();
+	} else {
+		document.getElementById("output").style.color = 'black';
+		document.getElementById("output2").style.color = 'black';
+		rollDice();
+	}
 }
-
-
